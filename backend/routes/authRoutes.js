f5c4b96cf4e537d24696975131e09bb17a7b5aa3
Singhskr1007 <121811278+Singhskr1007@users.noreply.github.com>
2025-06-10 21:03:49 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -19,7 +19,7 @@ router.post("/upload-image",upload.single("image"),(req,res) =>{
     {
         return res.status(400).json({message:"No File Uploaded"});
     }
-    const imageUrl=`${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
+    const imageUrl=`//${req.get('host')}/uploads/${filename}`;
     res.status(200).json({imageUrl});
 });
 
