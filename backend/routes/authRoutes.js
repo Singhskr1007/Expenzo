86d090f51671973ceab93bd284c7fc55e44340ec
Singhskr1007 <121811278+Singhskr1007@users.noreply.github.com>
2025-06-11 09:35:31 +0530

    Update authRoutes.js force https

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -14,13 +14,14 @@ router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/getUser", protect, getUserInfo);
 
-router.post("/upload-image",upload.single("image"),(req,res) =>{
-    if(!req.file)
-    {
-        return res.status(400).json({message:"No File Uploaded"});
+router.post("/upload-image", upload.single("image"), (req, res) => {
+    if (!req.file) {
+        return res.status(400).json({ message: "No File Uploaded" });
     }
-    const imageUrl=`${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
-    res.status(200).json({imageUrl});
+
+    // Force HTTPS
+    const imageUrl = `https://expenzo-backend-8zs9.onrender.com/uploads/${req.file.filename}`;
+    res.status(200).json({ imageUrl });
 });
 
 module.exports = router;
